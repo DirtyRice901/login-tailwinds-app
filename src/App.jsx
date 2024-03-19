3db0d9953659998bed3b019f0f4304e310594d20
Kevin Rice <reatriniti@gmail.com>
2024-03-19 19:23:39 -0400

    created main.jsx, renamed Form.jsx to Login.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import Form from "./components/Form";
+import { useState } from "react";
+import Login from "./components/Login";
 
 
 function App() {
   return (
     <div className="flex w-full h-screen">
       <div className="w-full flex items-center justify-center lg:w-1/2">
-      <Form /> 
+      <Login /> 
       </div>
       <div className="hidden relative lg:flex h-full w-1/2 items-center justify-center bg-pink-200">
         <div className="w-60 h-60 bg-gradient-to-tr from-violet-500 to-pink-500 rounded-full animate-bounce shadow-2xl"/>
