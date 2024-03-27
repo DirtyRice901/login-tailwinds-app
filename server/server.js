a0a37d861f70fd35aa62e57cdf44ed349785617c
Kevin Rice <reatriniti@gmail.com>
2024-03-26 20:18:58 -0400

    Update MongoDB connection string

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect("mongodb://127.0.0.1:27017/login-tailwindsDB");
+mongoose.connect("mongodb://127.0.0.1:27017/loginDB");
 
 app.post('/register', (req, res) => {
     const {name, email, password} = req.body;
